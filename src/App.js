7a853b672336c7080f9f8b7db3717315cf36cc3f
J.Daniel <juandaniel9619@gmail.com>
2022-07-24 11:29:01 -0400

    readme, fixed hero, added featured

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
 import { HomeView } from "./views/HomeView";
 import { Route, Routes } from "react-router-dom";
 import { NavBar } from "./components/navBar/navBar";
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 // import "bootstrap-css-only/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
